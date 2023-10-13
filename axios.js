acc82e6a2987231f64212e641f4ddb5bd877694f
Tuan Le Anh <tuanlegameagelayer@gmail.com>
2023-10-14 00:22:12 +0700

    Add env setting

--- axios.js
+++ axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.VUE_APP_BACKEND_URL,
    //   withCredentials: true
 });
 
